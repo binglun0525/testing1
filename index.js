29b981a8b1cf5b7cdb64dbd363fb244a250da9be
binglun0525 <86881307+binglun0525@users.noreply.github.com>
2021-07-03 22:29:27 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create express app
+const app = express();
+
+// app.use(bodyParser.json({
+//   limit: '200mb'
+// }));
+// app.use(bodyParser.urlencoded({
+//   limit: '200mb',
+//   extended: true
+// }));
+
+// define a simple route
+app.get('/', (req, res) => {
+  res.json({
+    "message": "Welcome to Testing application. "
+  });
+});
+
+
+app.listen(process.env.PORT || 5000)
