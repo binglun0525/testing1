11bd5e91a45ea924f2d9bd37b1059f52ec626142
binglun0525 <86881307+binglun0525@users.noreply.github.com>
2021-07-03 21:52:40 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create express app
+const app = express();
+
+app.use(bodyParser.json({
+  limit: '200mb'
+}));
+app.use(bodyParser.urlencoded({
+  limit: '200mb',
+  extended: true
+}));
+
+// define a simple route
+app.get('/', (req, res) => {
+  res.json({
+    "message": "Welcome to Testing application. "
+  });
+});
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  }).catch(err => {
+    console.log(err);
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+  });
+
+// Require Notes routes
+require('./app/routes/admin.routes.js')(app);
+
+
+// listen for requests
+app.listen(3001, () => {
+  console.log("Server is listening on port 3001");
+});
